b5029dfa07eadd1db3ac281aba6860125de8da33
Winstonshih <Winstonshih@github.com>
2026-08-14 01:16:46 +0000

    Test run: 13/13 passed, 0 failed, 0 skipped - 2026-08-14T01:16:46.614Z

--- src/main/java/Application/Service/BookService.java
+++ src/main/java/Application/Service/BookService.java
@@ -3,6 +3,8 @@ package Application.Service;
 import Application.DAO.BookDAO;
 import Application.Model.Book;
 
+import static org.mockito.ArgumentMatchers.nullable;
+
 import java.util.List;
 
 /**
@@ -51,7 +53,12 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
-        return bookDAO.getBookByIsbn(book.getIsbn());
+        if(bookDAO.getBookByIsbn(book.getIsbn())!=null)
+        {
+            return null;
+        }
+        bookDAO.insertBook(book);
+        return book;
     }
     /**
      * TODO: Use the bookDAO to retrieve a list of all books that have a bookCount above 0.
